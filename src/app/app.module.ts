864503df3353ee06eb47e7f715157581a2a34770
Kannan <713088@cognizant.com>
2018-08-22 16:03:22 +0200

    Signed-off-by: Angular router basics - Sujat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {
   MatToolbarModule,
   MatIconModule,
   MatCardModule
-  
+
 } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
@@ -20,14 +20,26 @@ import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-import {DishService} from './services/dish.service';
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +53,8 @@ import {DishService} from './services/dish.service';
     MatCardModule,
     MatListModule,
     FlexLayoutModule,
-    MatGridListModule
+    MatGridListModule, 
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
